77be5bb0f4bc81cf6630e6dfd12cb8e1884d1a35
AbigailBeck <65158716+AbigailBeck@users.noreply.github.com>
2020-07-28 15:41:40 +0300

    Delete index.js

--- client/worker/index.js
+++
@@ -1,6 +0,0 @@
-var CronJob = require('cron').CronJob;
-
-const fetchGithub = require('./tasks/fetch-github');
-
-var job = new CronJob('* * * * *', fetchGithub, null, true, 'America/Los_Angeles');
-
